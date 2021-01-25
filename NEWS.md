58cf2e2ad7837a90655b4310f2e57b5654f3b58f
Joe Roe <joe@joeroe.io>
2021-01-25 19:03:26 +0100

    Add this_year()

--- NEWS.md
+++ NEWS.md
@@ -12,6 +12,7 @@ Second beta / CRAN pre-release.
   * Now has a `precision` argument, allowing the result to be rounded (#23).
 * `era` and `yr` objects are now validated when constructed, using new functions `validate_era()`/`is_valid_era()` and `validate_yr()`/`is_valid_yr()` (#7 and #8).
 * Minor changes/bug fixes:
+  * Added `this_year()`, which returns the current year as a `yr` object.
   * `era(NA)` now returns an error, not a vector of all standard eras (#20).
   * All `era` arguments in functions can now accept a character vector (#20)
   * `era(<era>)` now returns an era with the same parameters (to enable the above)
---
+++ R/examples.R
@@ -0,0 +1,34 @@
+# examples.R
+# Miscellaneous functions for examples and documentation
+
+#' Current year
+#'
+#' Returns the current year as a year vector, in the era system specified by
+#' `era`.
+#'
+#' @param era An `era` object or label understood by [era()]. Defaults to the
+#'  Common Era (`era("CE")`).
+#'
+#' @return
+#' A `yr` vector with the current year.
+#'
+#' @family era helper functions
+#'
+#' @export
+#'
+#' @examples
+#' # This year in the Common Era
+#' this_year()
+#' # This year in the Holocene Epoch
+#' this_year("HE")
+this_year <- function(era = "CE") {
+  dst_era <- era(era)
+  this_year <- as.numeric(format(Sys.Date(), "%Y"))
+  this_year <- yr(this_year, "CE")
+
+  if (dst_era != era("CE")) {
+    this_year <- yr_transform(this_year, era(dst_era))
+  }
+
+  return(this_year)
+}
