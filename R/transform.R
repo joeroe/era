4f447cec303fe2b733a2034451df66d68224428e
Joe Roe <joe@joeroe.io>
2021-01-26 14:34:09 +0100

    Add term for converting year units to yr_transform()
    Should address #25 pending tests and documentation

--- R/transform.R
+++ R/transform.R
@@ -2,31 +2,34 @@
 
 #' Convert years from one era to another
 #'
-#' Transform a vector of years from one era to another. Not all transformations
-#' are possible (see details).
+#' Transform a vector of years from one era to another.
 #'
-#' @param x  A `yr` object to be transformed.
-#' @param era  Target `era` object, see [era()].
+#' @param x `yr` object. A vector of years with an era, see [yr()].
+#' @param era `era` object describing the target era, see [era()].
 #' @param precision Desired precision of the transformation, i.e. the
 #'  transformed values are rounded to the nearest `precision`. If `NA`
 #'  (the default), no rounding is performed and the exact transformed value is
 #'  returned.
 #'
 #' @details
-#' Era transformations currently supported:
+#' Transformation between eras uses the `scale`, `epoch`, `direction` and `unit`
+#' parameters of the era definition. `NA` values for any of these parameters in
+#' the source or destination era will cause an error. This is most often
+#' encountered when either are measured in 'radiocarbon years', which cannot be
+#' related to a calendar time scale without
+#' [calibration](https://en.wikipedia.org/wiki/Radiocarbon_calibration) or
+#' un-calibration. [stratigraphr::c14_calibrate()] can be used to calibrate
+#' radiocarbon dates preserving the `era` attribute.
 #'
-#' * Between scales
-#' * Between epochs
-#' * Between directions
+#' The transformation function is exact and treats years as a real number
+#' scale. This means that transformations between eras with different year
+#' units (e.g. Gregorian ↔ Julian) and/or epochs not aligned to 1 January
+#' in the Gregorian calendar (e.g. Common Era ↔ Islamic calendars) will likely
+#' return non-integer values. The `precision` argument provides a
+#' convenient way to round the result if you do not need this level of
+#' precision. It is also useful for working around the ambiguous definition of
+#' 'present' in various geological time-scales.
 #'
-#' Not supported:
-#'
-#' * Between different units (to be implemented).
-#' * units: `calendar` ↔ `radiocarbon` years. This requires
-#'   [calibration](https://en.wikipedia.org/wiki/Radiocarbon_calibration)
-#'   or un-calibration, which is outside the scope of this package. Functions
-#'   for radiocarbon calibration can be found in [rcarbon::calibrate()] and
-#'   [Bchron::BchronCalibrate()].
 #'
 #' @return
 #' A `yr` object in the era specified by `era`.
@@ -44,15 +47,19 @@ yr_transform <- function(x, era = yr_era(x), precision = NA) {
   src_era <- yr_era(x)
   dst_era <- era(era)
 
-  # Check comparable units
-  if (era_unit(src_era) != era_unit(dst_era)) {
+  # Check compatible units
+  na_units <- c(era_unit(src_era), era_unit(dst_era))
+  na_units <- na_units[is.na(era_year_days(na_units))]
+  if (length(na_units) == 1) {
+    na_units <- unique(na_units)
+    na_units <- paste0("Calendar length of a ", era_year_label(na_units),
+                       " year is undefined.")
+    names(na_units) <- rep("x", length(na_units))
     abort(
-      paste0("Can't transform era ", era_label(src_era), " to ", era_label(dst_era), ":"),
+      paste0("Cannot transform ", era_name(src_era), " to ",
+             era_name(dst_era), " years:"),
       class = "era_invalid_transform",
-      body = format_error_bullets(c(
-        x = paste0("Can't convert ", era_year_label(era_unit(src_era)), " to ",
-                   era_year_label(era_unit(dst_era)), " years.")
-        ))
+      body = format_error_bullets(na_units)
       )
   }
 
@@ -60,11 +67,13 @@ yr_transform <- function(x, era = yr_era(x), precision = NA) {
   x <- x * era_scale(src_era)
 
   # Transformation
+  ux <- era_year_days(era_unit(src_era))
+  uy <- era_year_days(era_unit(dst_era))
   dx <- era_direction(src_era)
   dy <- era_direction(dst_era)
   ex <- era_epoch(src_era)
   ey <- era_epoch(dst_era)
-  y <- x * (dx*dy) + dy * (ex - ey)
+  y <- (ux/uy) * (x * (dx*dy) + dy * (ex - ey))
 
   # Apply destination scale
   y <- y / era_scale(dst_era)
