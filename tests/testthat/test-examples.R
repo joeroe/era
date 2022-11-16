2bd77d6e1ae0aae4a0ba51fff33ac5e7e6619d0b
Joe Roe <git@joeroe.io>
2022-11-16 16:30:32 +0100

    Fix malfunctioning test
    The test "this_year() is vectorised" was causing R CMD check to fail because it relied on a transformation of the current year to AM, which depends on the current time of year. Switched to using only Gregorian eras. Fixes #41

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -13,9 +13,9 @@ test_that("this_year() rounds negative years correctly", {
 test_that("this_year() is vectorised", {
   year_now <- yr(lubridate::year(lubridate::now()), "CE")
   year_now_bp <- yr_transform(year_now, "BP", precision = 1)
-  year_now_am <- yr_transform(year_now, "AM", precision = 1)
+  year_now_b2k <- yr_transform(year_now, "b2k", precision = 1)
   expect_equal(
-    this_year(c("CE", "BP", "AM")),
-    list(year_now, year_now_bp, year_now_am)
+    this_year(c("CE", "BP", "b2k")),
+    list(year_now, year_now_bp, year_now_b2k)
   )
 })
